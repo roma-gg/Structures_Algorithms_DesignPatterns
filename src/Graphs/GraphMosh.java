4ad8a30daafbedc5920c490a2a4276c41eb60a82
Roman <rbaidachenko@gmail.com>
2023-11-11 18:30:24 +0000

    saving before removing ArrayList of LinkedLists from Graph.java

--- src/Graphs/GraphMosh.java
+++ src/Graphs/GraphMosh.java
@@ -1,4 +1,29 @@
 package Graphs;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class GraphMosh {
+    private class Node {
+        private String label;
+
+        public Node(String label) {
+            this.label = label;
+        }
+    }
+
+    private Map<String, Node> nodes = new HashMap<>();
+    private Map<Node, List<Node>> adjacencyList
+
+    public void addNode(String label) {
+        var node = new Node(label);
+        nodes.putIfAbsent(label, node);
+    }
+
+    public void addEdge (String from, String to) {
+        if (nodes.get(from) == null || nodes.get(to) == null)
+            throw new IllegalArgumentException();
+
+    }
 }
