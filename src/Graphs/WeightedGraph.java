5f78a4bbe3858c321f716d075ecd23f566c37b9a
Roman <rbaidachenko@gmail.com>
2023-11-15 22:59:55 +0000

    solved Dijkstra's algorithm

--- src/Graphs/WeightedGraph.java
+++ src/Graphs/WeightedGraph.java
@@ -1,30 +1,25 @@
 package Graphs;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class WeightedGraph {
     private class Node {
 
         private String label;
-
-        public List<Edge> getEdges() {
-            return edges;
-        }
-
         private List<Edge> edges = new ArrayList<>();
 
         public Node(String label) {
             this.label = label;
         }
 
+        public List<Edge> getEdges() {
+            return edges;
+        }
+
         public void addEdge(Node to, int weight) {
             var edge = new Edge(this, to, weight);
             edges.add(edge);
         }
-
 
         @Override
         public String toString() {
@@ -48,6 +43,16 @@ public class WeightedGraph {
         }
     }
 
+    private class NodeEntry {
+        private Node node;
+        private int priority;
+
+        public NodeEntry(Node node, int priority) {
+            this.node = node;
+            this.priority = priority;
+        }
+    }
+
     private Map<String, Node> nodes = new HashMap<>();
 
 
@@ -67,6 +72,77 @@ public class WeightedGraph {
         fromNode.addEdge(toNode, weight);
         toNode.addEdge(fromNode, weight);
     }
+
+    // map Node, Integer distances
+    // map Node, Node previousNodes
+    // for each neighbour
+    // if found shorter path
+    // update distances table
+    // push neighbour to queue
+
+    public int getShortestDistance(String from, String to) {
+        PriorityQueue<NodeEntry> queue = new PriorityQueue<>(Comparator.comparing(m -> m.priority));
+        var distances = new HashMap<Node, Integer>();
+        var previousNode = new HashMap<Node, Node>();
+
+        for (var node : nodes.values()) {
+            distances.put(node, Integer.MAX_VALUE);
+        }
+
+        var fromNode = nodes.get(from);
+        distances.put(fromNode, 0);
+
+        getShortestDistance(fromNode, distances, previousNode, queue);
+
+        var toNode = nodes.get(to);
+        return distances.get(toNode);
+
+
+    }
+
+    //from A to D
+    private void getShortestDistance(Node current, Map<Node, Integer> distances,
+                                     Map<Node, Node> previousNode, PriorityQueue<NodeEntry> queue) {
+
+        for (var edge : current.getEdges()) {
+            Node child = edge.to;
+
+            int distanceToChild = edge.weight + getDistanceFromStart(current, distances, previousNode);
+
+            if (!previousNode.containsValue(child) && distances.get(child) > distanceToChild) {
+                distances.put(child, distanceToChild);
+                previousNode.put(child, current);
+                queue.add(new NodeEntry(child, distanceToChild));
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            getShortestDistance(queue.remove().node, distances, previousNode, queue);
+        }
+    }
+
+    // B 1 E
+    // D 5 E
+    //
+
+    //getting shortest distance to the child Node
+    private int getDistanceFromStart(Node current, Map<Node, Integer> distances, Map<Node, Node> previousNode) {
+        int distance = 0;
+        while (previousNode.containsKey(current) &&
+                distances.get(current) != Integer.MAX_VALUE) {
+            distance += distances.get(current);
+            current = previousNode.get(current);
+        }
+        return distance;
+    }
+
+    private void clearDistanceTrace(HashMap<Node, Integer> distances, Map<Node, Node> previousNode, PriorityQueue<NodeEntry> queue) {
+        distances.clear();
+        previousNode.clear();
+        queue.clear();
+    }
+
+
 
     public void print() {
         for (var node : nodes.values()) {
--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,18 @@ public class Main {
         graph.addNode("A");
         graph.addNode("B");
         graph.addNode("C");
+        graph.addNode("D");
+        graph.addNode("E");
         graph.addEdge("A", "B", 3);
-        graph.addEdge("A", "C", 2);
+        graph.addEdge("A", "C", 4);
+        graph.addEdge("A", "D", 2);
+        graph.addEdge("C", "D", 1);
+        graph.addEdge("B", "D", 6);
+        graph.addEdge("B", "E", 1);
+        graph.addEdge("D", "E", 5);
 
-        graph.print();
+        System.out.println(graph.getShortestDistance("B", "A"));
+
 
 
 
